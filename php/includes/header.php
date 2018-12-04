b4ab414342e07688525230295898ad720e943c05
Lucaprozz <1038179@idcollege.nl>
2018-12-04 16:24:45 +0100

    created folder structure

---
+++ php/includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../css/style.css">
+    <title>Document</title>
+</head>
+<header class="header">
+	<nav class="nav">
+		<ul class="navigation">
+			<li class="navigation__list"><a class="navigation__link">Home</a></li>
+			<li class="navigation__list"><a class="navigation__link">Diensten</a></li>
+			<li class="navigation__list"><a class="navigation__link">Organisatie</a></li>
+			<li class="navigation__list"><a class="navigation__link">Klanten</a></li>
+			<li class="navigation__list"><a class="navigation__link">Projecten</a></li>
+			<li class="navigation__list"><a class="navigation__link">Offerte</a></li>
+			<li class="navigation__list"><a class="navigation__link">Contact</a></li>
+		</ul>
+	</nav>
+</header>
